c5f9de905ff916e5a230add2ad489a72e7c5094e
Joy C <jcdangdesigns@gmail.com>
2022-01-16 18:21:42 -0800

    Next.js Login Page
    Sign in page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId="DH7ojo9rkn2t3SWF4LRAwoWqwKLp3hC9OS1t6TQk"
+      serverUrl="https://q9gr27nlmw68.usemoralis.com:2053/server"
+    >
+      {<Component {...pageProps} />}
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
